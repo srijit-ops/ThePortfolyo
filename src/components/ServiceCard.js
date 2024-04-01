1df1d6358f5f1093d38e8d5fdba9073e9f7808ee
srijit-ops <srijitasengupta23@gmail.com>
2024-04-01 22:39:06 +0530

    card change

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -5,8 +5,8 @@ import Button from './common/Button';
 
 function ServiceCard({enabled, img, name, price, detail}) {
   return (
-    <div className={`bg-white shadow-[rgba(99,99,99,0.2)_0px_2px_8px_0px] transition-all duration-[0.3s] m-8 p-6 rounded-[10px] hover:shadow-[rgba(50,50,93,0.25)_0px_13px_27px_-5px,rgba(0,0,0,0.3)_0px_8px_16px_-8px] cursor-pointer`}>
-    <div className={`${Styles.image} overflow-hidden relative rounded-[10px] w-full border-gray-200 border`}>
+    <div className={`bg-white dark:bg-[#363636] shadow-[rgba(99,99,99,0.2)_0px_2px_8px_0px] hover:scale-105 transition-all duration-[0.3s] m-8 p-6 rounded-[10px] hover:shadow-[rgba(50,50,93,0.25)_0px_13px_27px_-5px,rgba(0,0,0,0.3)_0px_8px_16px_-8px] cursor-pointer`}>
+    <div className={`${Styles.image} overflow-hidden relative rounded-[10px] w-full border-gray-200 dark:border-gray-600 border`}>
       <Image
         src={img}
         alt={name}
@@ -17,7 +17,7 @@ function ServiceCard({enabled, img, name, price, detail}) {
       />
     </div>
     <div className="text-center p-3">
-      <h1 className='text-xl text-gray-950 font-semibold mx-auto my-2'>{name}</h1>
+      <h1 className='text-xl text-gray-950 dark:text-white font-semibold mx-auto my-2'>{name}</h1>
       <div className="tracking-wider inline-block  mx-2 my-1 px-4 py-2 rounded-[50px] bg-[#FF69B4] text-white">
         <p>{price}</p>
             {/* <Tabs
@@ -27,7 +27,7 @@ function ServiceCard({enabled, img, name, price, detail}) {
             /> */}
           
       </div>
-      <p className='text-gray-600 mt-2 text-sm'>{detail}</p>
+      <p className='text-gray-600 dark:text-gray-300 mt-2 text-sm'>{detail}</p>
       <div className='flex justify-center items-center'>
       <Button name={"View"}/>
       </div>
