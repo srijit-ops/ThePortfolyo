334c8147f4a68b94951508abd5999784aa1bfb5d
srijit-ops <srijitasengupta23@gmail.com>
2024-04-02 20:45:58 +0530

    added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" class="dark">
-      <Head/>
-     
+      <Head />
       <body class="dark:bg-black">
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import { getPortfolioData } from "@/requests/portfolioDataRequest.requests";
-import { useTheme } from 'next-themes';
-import Image from "next/image";
 import { useState } from "react";
 import Head from "next/head";
 import HeroSection from "@/components/HeroSection";
@@ -14,64 +12,63 @@ import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-
-export default function Home({data}) {
-
-  const [loading, setLoading] = useState(false)
+export default function Home({ data }) {
+  const [loading, setLoading] = useState(false);
   // console.log(data, 'prop check')
-  const { theme, setTheme } = useTheme();
 
-  // console.log(theme, setTheme, "context check")
-  
   return (
     <>
       <Head>
         <title>{data.user.about.name}</title>
-        <meta property='og:title' content={data.user.about.name} />
-        <meta
-          property='og:image'
-          content={data.user.about.avatar.url}
+        <meta property="og:title" content={data.user.about.name} />
+        <meta property="og:image" content={data.user.about.avatar.url} />
+        <meta property="og:description" content={data.user.about.subTitle} />
+        <meta name="description" content={data.user.about.subTitle}></meta>
+        <link
+          rel="shortcut icon"
+          href={data.user.about.avatar.url}
+          type="image/x-icon"
         />
-        <meta
-          property='og:description'
-          content={data.user.about.subTitle}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins&family=Caveat&display=swap"
+          rel="stylesheet"
         />
-        <meta
-          name='description'
-          content={data.user.about.subTitle}
-        ></meta>
-        <link rel='shortcut icon' href={data.user.about.avatar.url} type='image/x-icon' />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Poppins&family=Caveat&display=swap" rel="stylesheet"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js" integrity="sha512-GWzVrcGlo0TxTRvz9ttioyYJ+Wwk9Ck0G81D+eO63BaqHaJ3YZX9wuqjwgfcV/MrB2PhaVX9DkYVhbFpStnqpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> */}
-        
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js" integrity="sha512-GWzVrcGlo0TxTRvz9ttioyYJ+Wwk9Ck0G81D+eO63BaqHaJ3YZX9wuqjwgfcV/MrB2PhaVX9DkYVhbFpStnqpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> */}
       </Head>
-      <main
-      className={`pb-8  bg-white dark:bg-black relative`}
-    >
-      <Header name={data.user.about.name}/>
-      <HeroSection heroData={data.user.about}/>
-        <AboutSection aboutData={data.user.about}/>
-        <SkillSection skillData= {data.user.skills}/>
-        <ServiceSection serviceData={data.user.services}/>
-        <ProjectSection projectData={data.user.projects}/>
-        <TimelineSection timelineData={data.user.timeline}/>
-        <TestimonialSection testimonialData={data.user.testimonials}/>
-        <ContactSection email={data.user.about.contactEmail} location={data.user.about.address} phone={data.user.about.phoneNumber}/>
-        <Footer socials={data.user.social_handles}/>
-    </main>
+      <main className={`pb-8  bg-white dark:bg-black relative`}>
+        <Header name={data.user.about.name} />
+        <HeroSection heroData={data.user.about} />
+        <AboutSection aboutData={data.user.about} />
+        <SkillSection skillData={data.user.skills} />
+        <ServiceSection serviceData={data.user.services} />
+        <ProjectSection projectData={data.user.projects} />
+        <TimelineSection timelineData={data.user.timeline} />
+        <TestimonialSection testimonialData={data.user.testimonials} />
+        <ContactSection
+          email={data.user.about.contactEmail}
+          location={data.user.about.address}
+          phone={data.user.about.phoneNumber}
+        />
+        <Footer socials={data.user.social_handles} />
+      </main>
     </>
-    
   );
 }
 
-export const getServerSideProps= async ()=>{
-  const {data}= await getPortfolioData()
-  return{
-    props:{
-      data:data
-    }
-  }
-}
+export const getServerSideProps = async () => {
+  const { data } = await getPortfolioData();
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
